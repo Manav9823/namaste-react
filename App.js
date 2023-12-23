4ad7bc168b60da867ef4e721feb29e11c1e81250
Manav9823 <manavbansalfddk@gmail.com>
2023-12-23 12:39:16 +0530

    parcel implementation

--- App.js
+++ App.js
@@ -1,4 +1,10 @@
 /* eslint-disable no-undef */
+
+
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
 const heading1 = React.createElement("h1", {id: "title"}, "Namaste Everyone");
 const heading2 = React.createElement("h2", {id: "title"}, "Namaste Everyone2");
 const div = React.createElement("div", {id :"container"}, [heading1, heading2]);
